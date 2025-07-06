3365fd0428c8033f6c119c554bb3137755c553cb
haru <chocomos_tea86@ezweb.ne.jp>
2025-07-06 13:15:19 +0900

    #15 pictgramへのリファクタリング

--- src/main/java/com/example/pictgram2/entity/UserInf.java
+++
@@ -1,8 +0,0 @@
-package com.example.pictgram2.entity;
-
-public interface UserInf {
-
-	Long getUserId();
-
-	String getUsername();
-}
--- src/main/java/com/example/pictgram2/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pictgram2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.pictgram2.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
